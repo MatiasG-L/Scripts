c5eb9c462a97108d56e0ecdc0660d17adef4e1bc
Matias G-L <150760583+MatiasG-L@users.noreply.github.com>
2023-11-15 10:30:15 -0600

    Update EnemyAI
    removed unused collision method

--- EnemyAI.cs
+++ EnemyAI.cs
@@ -86,16 +86,6 @@ public class EnemyAI : MonoBehaviour
         }
 
     }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "slashSpot")
-        {
-          
-            
-            
-        }
-    }
 
- 
 
 }
